687dde3e8334794ec59da2fd0e27df29428c00d2
Akash Gupta <gr8akash1994@gmail.com>
2024-05-13 23:09:00 +0530

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'painter-portfolio';
+  @ViewChild('video1') video1Ref!: ElementRef;
+  @ViewChild('video2') video2Ref!: ElementRef;
+  @ViewChild('video3') video3Ref!: ElementRef;
+  @ViewChild('video4') video4Ref!: ElementRef;
+
+  ngOnInit(): void {
+    // Play the videos programmatically
+    this.video1Ref.nativeElement.play();
+    this.video2Ref.nativeElement.play();
+    this.video3Ref.nativeElement.play();
+    this.video4Ref.nativeElement.play();
+    
+  }
 }
