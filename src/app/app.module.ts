67f0d3e9c5e160437447855832531b4c1749887c
Akash Gupta <gr8akash1994@gmail.com>
2024-05-13 19:42:33 +0530

    For Painter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { ImageComponent } from './pictures/image/image.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
